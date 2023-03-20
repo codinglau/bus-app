513bc8cc1fff7ac3163a915ec99e09c156aa2ec2
codinglau <codinglau@gmail.com>
2023-03-21 00:25:34 +0800

    update public path

--- quasar.config.js
+++ quasar.config.js
@@ -68,7 +68,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/transportation-app',
+      publicPath: '/transportation-app',
       // analyze: true,
       env: {
         API: {
